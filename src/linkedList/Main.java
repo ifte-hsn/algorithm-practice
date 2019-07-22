62d4a1c3311e85ab6d716271dfe3146858b0169c
ifte.hsn <ifte.hsn@gmail.com>
2019-07-22 12:45:32 +0600

    implemented linked list

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -5,83 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		SList list = new SList();
 		
-		System.out.println("List empty? "+ list.empty());
-		list.pushFront(5);
 		
-		System.out.println("List empty? "+ list.empty());
 		
-		list.pushFront(4);
-		list.pushFront(2);
-		list.pushFront(1);
+		list.pushBack(4);
+		list.pushBack(3);
+		list.pushBack(2);
+		list.pushBack(1);
+		
 		
 		list.printList();
-		
-		System.out.println("-----------------");
-		System.out.println("     INSERT      ");
-		System.out.println("-----------------");
-		list.insert(3, 2);
-		
-		list.printList();
-		
-		
-		System.out.println("-----------------");
-		System.out.println("    PUSH BACK    ");
-		System.out.println("-----------------");
-		
-		list.pushBack(7);
-		
-		list.printList();
-		
-		
-		System.out.println("-----------------");
-		System.out.println("     INSERT      ");
-		System.out.println("-----------------");
-		
-		list.insert(6, 5);
-		list.printList();
-		
-		System.out.println("-----------------");
-		System.out.println("     INSERT      ");
-		System.out.println("-----------------");
-		
-		list.insert(7, 5);
-		list.printList();
-		//System.out.println("list size "+ list.size());
-		
-		//System.out.println("value at 4 is "+ list.valueAt(4));
-		
-		System.out.println("-----------------");
-		System.out.println("     ERASE       ");
-		System.out.println("-----------------");
-		list.erase(5);
-		list.printList();
-		
-		System.out.println("--------------------");
-		System.out.println("      POP FRONT     ");
-		System.out.println("--------------------");
-		list.popFront();
-		list.printList();
-		System.out.println("size "+list.size());
-		
-		
-		System.out.println("-------------------");
-		System.out.println("      POP BACK     ");
-		System.out.println("-------------------");
-		list.popBack();
-		list.printList();
-		System.out.println("size "+list.size());
-		
-		System.out.println("----------------");
-		System.out.println("      FRONT     ");
-		System.out.println("----------------");
-		
-		System.out.println(list.front());
-		
-		System.out.println("---------------");
-		System.out.println("      BACK     ");
-		System.out.println("---------------");
-		
-		System.out.println(list.back());
+	
 		
 	}
 
--- src/linkedList/SLinkedList.java
+++ src/linkedList/SLinkedList.java
@@ -1,6 +1,5 @@
 package linkedList;
 
-import javax.swing.plaf.SliderUI;
 
 class SList {
 	private static SLinkedListNode head;
@@ -9,82 +8,95 @@ class SList {
 	public int size() {
 		return size;
 	}
-
+	
 	public boolean empty() {
 		return size == 0;
 	}
 	
 	public Object valueAt(int index) {
-		if(index >= size) {
+		if(index < 1 || index > size) {
 			throw new IndexOutOfBoundsException();
 		}
 		
 		SLinkedListNode temp = head;
 		
-		for(int i = 0; i<=index-1; i++) {
+		for(int i = 1; i < index-1; i++) {
 			temp = temp.next;
 		}
 		
 		return temp.item;
 	}
-
+	
 	public void pushFront(Object item) {
 		head = new SLinkedListNode(item, head);
 		size++;
 	}
-
+	
 	public Object popFront() {
-		if(head.next != null ) {
-			head = head.next;
-			size--;
-			return head.item;
+		if(size == 0) {
+			return null;
 		}
-		return null;
+		
+		Object item = head.item;
+		head = head.next;
+		size--;
+		
+		return item;
 	}
-
+	
 	public void pushBack(Object item) {
 		SLinkedListNode temp = head;
+		
+		
+		if(temp == null) {
+			head = new SLinkedListNode(item);
+			return;
+		}
+		
 		while(temp.next != null) {
 			temp = temp.next;
 		}
 		
 		temp.next = new SLinkedListNode(item);
+		
 		size++;
 	}
-
-	public Object popBack(){
-		SLinkedListNode current = head;
-		SLinkedListNode prev = null;
+	
+	public Object popBack() {
+		if(size == 0) {
+			return null;
+		}
 		
+		SLinkedListNode prev = null;
+		SLinkedListNode current = head;
 		while(current.next != null) {
 			prev = current;
 			current = current.next;
 		}
 		prev.next = null;
 		size--;
-		
-		return prev.item;
-
+		return current.item;
 	}
 	
 	public Object front() {
 		return head.item;
 	}
-
+	
 	public Object back() {
 		SLinkedListNode temp = head;
 		
 		while(temp.next != null) {
 			temp = temp.next;
 		}
+		
 		return temp.item;
 	}
 	
 	public void insert(Object item, int index) {
-		SLinkedListNode prev = null;
 		SLinkedListNode current = head;
-	
-		for(int i = 0; i < index; i++) {
+		SLinkedListNode prev = null;
+		
+		for(int i = 1; i<index; i++) {
 			prev = current;
 			current = current.next;
 		}
@@ -93,10 +105,14 @@ class SList {
 	}
 	
 	public void erase(int index) {
+		
+		if(size == 0) {
+			return;
+		}
+		
 		SLinkedListNode prev = null;
 		SLinkedListNode current = head;
-	
-		for(int i = 0; i<index; i++) {
+		for(int i = 1; i < index-1; i++) {
 			prev = current;
 			current = current.next;
 		}
@@ -104,14 +120,41 @@ class SList {
 		size--;
 	}
 	
-	public void printList() {
-		SLinkedListNode temp = head;
+	public Object valueNFromEnd(int n) {
+		if(n > size || n < 1) {
+			throw new IndexOutOfBoundsException();
+		}
 		
-		while(temp != null) {
-			System.out.println(temp.item);
+		SLinkedListNode temp = head;
+		int index = size - n;
+		for(int i = 0; i <= index-1; i++) {
 			temp = temp.next;
 		}
+		return temp.item;
 	}
 	
+	public void reverse() {
+		SLinkedListNode prev = null;
+		SLinkedListNode next = null;
+		SLinkedListNode current = head;
+		
+		while(current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
 	
+	public void printList() {
+		SLinkedListNode temp = head;
+		int i = 0;
+		
+		while(temp.next != null) {
+			System.out.println(i+" => "+temp.item);
+			temp = temp.next;
+			i++;
+		}
+	}
 }
